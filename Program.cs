ae08e0185dbe427952a7339795c6f9a831aba733
bdbblowe <bdbblowe@gmail.com>
2018-01-30 20:51:04 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Title: Converter App");
+            Console.WriteLine("Description: Will convert yards to inches");
+            Console.WriteLine("Please enter the number of yards:");
+
+            int numYards = int.Parse(Console.ReadLine());
+            int numbInches = numYards * 36;
+
+            Console.WriteLine("Number of inches in " + numYards + " inches");
+
+            Console.ReadLine();
+        }
+    }
+}
